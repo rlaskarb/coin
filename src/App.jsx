8bdf79f5246f9a838e62bc9979a0279f8844be5b
Namkyu <rlaskarb20@gmail.com>
2025-11-19 22:27:28 +0900

    포멧후 셋팅완료

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <button onClick={() => setCount(count + 100)}> 가격 올리기 </button>
       <button onClick={() => setCount(count - 100)}> 가격 내리기 </button>
 
-      <p>리엑트로 코인매수매매를 만들어봅시다!</p>
+      <p>리엑트로 코인매수매매를 만들어봅시다!!</p>
     </div>
   );
 }
