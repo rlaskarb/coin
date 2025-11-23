fb256bda449d9e2987dca71c4abb2f463208cebf
Namkyu <rlaskarb20@gmail.com>
2025-11-23 23:57:40 +0900

    11/23 메인 비주얼 작업

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,30 +4,34 @@ import "./Header.css";
 const Header = () => {
   return (
     <header className="header-container">
-      <div className="logo">
-        <h1>upbit</h1>
-      </div>
+      <div className="header-inner">
+        <div className="logo">
+          <Link to="/">
+            <h1>upbit</h1>
+          </Link>
+        </div>
 
-      <nav className="gnb">
-        <ul>
-          <li>
-            <Link to="/exchange">거래소</Link>
-          </li>
-          <li>
-            <Link to="/deposit">입출금</Link>
-          </li>
-          <li>
-            <Link to="/investment">투자내역</Link>
-          </li>
-          <li>
-            <Link to="/movement">코인동향</Link>
-          </li>
-        </ul>
-      </nav>
+        <nav className="gnb">
+          <ul>
+            <li>
+              <Link to="/exchange">거래소</Link>
+            </li>
+            <li>
+              <Link to="/deposit">입출금</Link>
+            </li>
+            <li>
+              <Link to="/investment">투자내역</Link>
+            </li>
+            <li>
+              <Link to="/movement">코인동향</Link>
+            </li>
+          </ul>
+        </nav>
 
-      <div className="util-menu">
-        <button className="btn-login">로그인</button>
-        <button className="btn-signup">회원가입</button>
+        <div className="util-menu">
+          <button className="btn-login">로그인</button>
+          <button className="btn-signup">회원가입</button>
+        </div>
       </div>
     </header>
   );
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -9,10 +9,34 @@ function MainPage() {
 
       <main className="main-container">
         <section className="visual">
-          <h2>비주얼 영역</h2>
+          <h2 className="blind">비주얼 영역</h2>
+          <div className="visual-content">
+            <div className="visual-text">
+              <p>
+                가장 신뢰받는 거래소 <br />
+                글로벌 디지털 자산 거래소 <br />
+                <strong>업비트</strong>
+              </p>
+              <div className="text-btn">
+                <Link to="/exchange" className="textbtn btn1">
+                  거래소 둘러보기
+                </Link>
+                <button className="textbtn btn2">로그인</button>
+              </div>
+            </div>
+            <div className="visual-img">
+              <img src="/map.avif" alt="map" />
+              <div className="upbit-pin">
+                <div className="upbitimg">
+                  <img src="/visualUPbit.svg" alt="upbit" />
+                </div>
+                <i class="fa-solid fa-location-dot"></i>
+              </div>
+            </div>
+          </div>
         </section>
         <section className="trading-box">
-          <h2>거래량 영역</h2>
+          <h2 className="blind">거래량</h2>
           <ul>
             <li className="active">
               <Link to="/trading">거래량</Link>
