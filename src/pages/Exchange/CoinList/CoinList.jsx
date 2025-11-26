18fc14c736940751d7676d1b6425549571af533d
Namkyu <rlaskarb20@gmail.com>
2025-11-26 21:12:58 +0900

    11/26

--- src/pages/Exchange/CoinList/CoinList.jsx
+++ src/pages/Exchange/CoinList/CoinList.jsx
@@ -3,16 +3,27 @@ import "./CoinList.css";
 
 function CoinList() {
   const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchCoins = async () => {
+    const fetchMarkeDate = async () => {
       try {
-        const response = await fetch(
+        // 업비트에서 모든코인 이름(코드) 가져오기
+        const marketResponse = await fetch(
           "https://api.upbit.com/v1/market/all?isDetails=false"
         );
-        const data = await response.json();
+        const marketData = await response.json();
 
-        const krwCoins = data.filter((coin) => coin.market.includes("KRW-"));
+        // 원화로 시작하는 마켓만 남기기
+        const krwMarkets = marketData.filter((coin) =>
+          coin.market.startsWith("KRW-")
+        );
+
+        // 시세조회를 위해 마켓 코드만 뽑아서 콤마로 연결
+        const marketCodes = krwMarkets.map((coin) => coin.market).join(",");
+
+        //
+
         const top10 = krwCoins.slice(0, 10);
         setCoins(top10);
         console.log("가져온 코인 목록", top10);
